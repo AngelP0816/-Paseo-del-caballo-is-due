3157df552f007320a7a3b05d358e4797d1d422ec
AngelP0816 <106571057+AngelP0816@users.noreply.github.com>
2022-07-04 22:45:27 -0600

    Update Caballo.java

--- Caballo.java
+++ Caballo.java
@@ -1,7 +1,6 @@
 public class Caballo {
         
-    private int[][] tablero=new int[8][8];      //tablero en el que se llevara el registro del recorrido del caballo
-
+    private int[][] tablero=new int[8][8];     
     public Caballo() {
     }
     
@@ -9,30 +8,30 @@ public class Caballo {
         boolean result=false;
         if(i>=0 && j>=0 && i<tablero[0].length && j<tablero[0].length){
             if(tablero[i][j]==0){
-                result=true;        //metodo que valida la posición a la
-            }                       //que intenta moverse el caballo
+                result=true;        
+            }                      
         }
         return result;
     }
     
     public boolean recorre(int i, int j, int cont){
         boolean result=false;
-        tablero[i][j]=cont;                             //metodo que lleva a
-        if(cont==64){                                   //cabo el recorrido
-            result=true;                                //del caballo por el
-        }                                               //tablero y revisando
-        if(movValido(i-1, j+2)){                        //la posición a la
-            if(recorre(i-1, j+2, cont+1)){              //cual es mas factible
-                result=true;                            //moverse revisando
-            }                                           //las posibles posiciones
-        }                                               //a la cuales puede
-        if(movValido(i-2, j+1)){                        //moverse el caballo
-            if(recorre(i-2, j+1, cont+1)){              //de la siguiente manera
-                result=true;                            //  3   2
-            }                                           //4       1
-        }                                               //    X
-        if(movValido(i-2, j-1)){                        //5       8
-            if(recorre(i-2, j-1, cont+1)){              //  6   7
+        tablero[i][j]=cont;                             
+        if(cont==64){                                  
+            result=true;                                
+        }                                              
+        if(movValido(i-1, j+2)){                       
+            if(recorre(i-1, j+2, cont+1)){             
+                result=true;                            
+            }                                           
+        }                                             
+        if(movValido(i-2, j+1)){                        
+            if(recorre(i-2, j+1, cont+1)){             
+                result=true;                           
+            }                                         
+        }                                              
+        if(movValido(i-2, j-1)){                        
+            if(recorre(i-2, j-1, cont+1)){             
                 result=true;
             }
         }
@@ -68,13 +67,13 @@ public class Caballo {
     }
     
     public void imprimir(){
-        for(int i=0;i<tablero.length;i++){                  //metodo que imprime
-            for(int j=0;j<tablero[0].length;j++){           //el tablero para
-                System.out.print(tablero[i][j]+"\t");       //mostrar el recorrido
-                if(j==7){                                   //que llevo a cabo el
-                    System.out.println("\n");               //caballo una vez que
-                }                                           //este a finalizado su
-            }                                               //recorrido por el tablero
+        for(int i=0;i<tablero.length;i++){                  
+            for(int j=0;j<tablero[0].length;j++){          
+                System.out.print(tablero[i][j]+"\t");       
+                if(j==7){                                   
+                    System.out.println("\n");               
+                }                                           
+            }                                              
         }
     }
     
